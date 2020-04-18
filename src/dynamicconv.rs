896e89be7b04b82124374eb1eb3222d0633be136
Audun Hoem <audun.hoem@gmail.com>
2020-04-18 21:50:51 +0200

    dynamicconv.rs: implement Converse trait using LoginForm

--- src/dynamicconv.rs
+++ src/dynamicconv.rs
@@ -1,22 +1,103 @@
-impl Converse for Inputs<'_> {
-    
-    fn prompt_echo(&mut self, _: &CStr) -> Result<CString, ()> {
-         unimplemented!()
+use std::ffi::{
+    CStr,
+    CString,
+};
+use std::os::raw::c_char;
+use crate::loginform::LoginForm;
+use tui::{
+    backend::Backend, 
+    Terminal,
+};
+use pam::Converse;
+
+
+
+pub struct DynamicConv<'a, B> 
+where
+    B: Backend
+{
+    username: Option<String>,
+    form : LoginForm,
+    term: &'a mut Terminal<B>
+}
+
+impl<'a, B> DynamicConv<'a, B>
+where
+    B: Backend
+{
+    pub fn new(term: &'a mut Terminal<B>) -> DynamicConv<'a, B> {
+        DynamicConv {
+            username : None,
+            form: LoginForm::default(),
+            term,
+        }
     }
     
-    fn prompt_blind(&mut self, _: &CStr) -> Result<CString, ()> {
-        unimplemented!()
+    pub fn error_string(&mut self, error: String) {
+        self.form.error(error);
+    }
+}
+
+impl<B> Converse for DynamicConv<'_, B>
+where
+    B: Backend
+{
+    
+    fn prompt_echo(&mut self, msg: &CStr) -> Result<CString, ()> {
+        let title = msg.to_string_lossy().to_string();
+        self.form.add_input(title.clone(), false);
+        
+        if let Ok(input) = self.form.get_next_input(self.term) {
+            if title == "login:" {
+                self.username = Some(input.clone());
+            }
+            
+            if let Ok(ret) = CString::new(input) {
+                Ok(ret)
+            } else {
+                Err(())
+            }
+        } else {
+            Err(())
+        }
     }
     
-    fn info(&mut self, _: &CStr) {
-        unimplemented!()
+    fn prompt_blind(&mut self, msg: &CStr) -> Result<CString, ()> {
+        self.form.add_input(msg.to_string_lossy().to_owned().to_string(), true);
+        if let Ok(input) = self.form.get_next_input(self.term) {
+            if let Ok(ret) = CString::new(input) {
+                Ok(ret)
+            } else {
+                Err(())
+            }
+        } else {
+            Err (())
+        }
     }
     
-    fn error(&mut self, _: &CStr) {
-        unimplemented!()
+    fn info(&mut self, msg: &CStr) {
+        self.form.message(
+            msg.to_string_lossy()
+                .to_owned()
+                .to_string()
+        );
+        self.form.draw_form(self.term); 
+    }
+    
+    fn error (&mut self, msg: &CStr) {
+        self.form.error(
+            msg.to_string_lossy()
+                .to_owned()
+                .to_string()
+        );
+        self.form.draw_form(self.term);
     }
     
     fn username(&self) -> &str {
-        unimplemented!() 
+        if let Some(ret) = &self.username {
+            &ret
+        } else {
+            ""
+        }
     }
 }
